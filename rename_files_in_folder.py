abffa58641749f0c0c0c3a94ea6944f6d89f9014
Asfandyar-Khan-2022 <117299102+Asfandyar-Khan-2022@users.noreply.github.com>
2023-06-05 20:53:09 +0100

    face detection created

---
+++ rename_files_in_folder.py
@@ -0,0 +1,23 @@
+# Python program to rename all file
+# names in your directory
+import os
+
+def change_names(files_location, rename_to):
+    '''
+    Python program to rename all file 
+    names in your directory
+
+    :param files_location: locate the folder 
+    containing the files 
+
+    :param rename_to: choose the name you 
+    wish to have before the number
+    '''
+    os.chdir(files_location)
+
+    for count, f in enumerate(os.listdir()):
+        f_name, f_ext = os.path.splitext(f)
+        f_name = rename_to + str(count)
+    
+        new_name = f'{f_name}{f_ext}'
+        os.rename(f, new_name)
